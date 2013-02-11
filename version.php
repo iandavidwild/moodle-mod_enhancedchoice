14a8eef8678e8385f5e37c339c7bf01f76f6b8c6
ian.wild <ian.wild@heavy-horse.co.uk>
2013-02-11 21:59:23 +0000

    Updated version number.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013020400;       // The current module version (Date: YYYYMMDDXX)
+$module->version   = 2013020401;       // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2011070100;    // Requires this Moodle version (2.1)
 $module->component = 'mod_enhancedchoice';     // Full name of the plugin (used for diagnostics)
 $module->maturity  = MATURITY_ALPHA;	// How stable the plugin is
-$module->release   = '0.1 (Build: 2013020900)';  // Human-readable version name
+$module->release   = '0.2 (Build: 2013021000)';  // Human-readable version name
 $module->cron      = 0;
